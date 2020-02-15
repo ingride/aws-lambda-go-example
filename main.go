9c5f497fa0fab882a3c8749093026a48bfca9651
ingride <ingrid@netlify.com>
2020-02-15 02:24:30 +0000

    func update

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "💕 🌷 💕 Hello AWS Lambda and Netlify",
+		Body:       "⚡️ 💕 🌷 💕 Hello AWS Lambda and Netlify",
 	}, nil
 }
 
