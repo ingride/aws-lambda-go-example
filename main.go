81063f5f44c4e9ba8fca696e83100e3002472713
ingride <ingrid@netlify.com>
2020-02-16 12:51:27 +0000

    pushing a commit

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "⚡️ 💕 🌷 💕 Hello AWS Lambda and Netlify",
+		Body:       "♥️ ⚡️ 💕 🌷 💕 Hello AWS Lambda and Netlify",
 	}, nil
 }
 
